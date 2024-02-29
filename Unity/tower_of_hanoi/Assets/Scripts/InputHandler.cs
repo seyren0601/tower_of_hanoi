fcd7aa90d856c514b86aac90c687a8185d0141e2
Tom <dstart579@gmail.com>
2024-02-29 08:22:33 +0700

    main-scene-part-1
    28-2-2024

---
+++ Unity/tower_of_hanoi/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Camera _mainCamera;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+
+        if (!context.started) return;
+
+        var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+
+        if (!rayHit.collider) return;
+
+        Debug.Log(rayHit.collider.gameObject.name);
+    }
+}
